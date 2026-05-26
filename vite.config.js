ebea085eee946fb978bc2d8e3bfb09077bcc7e0f
Geroge <xx312472644@163.com>
2026-05-26 17:29:14 +0800

    build(vite): 配置基础路径适配生产和开发环境
    根据运行模式设置base路径

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,12 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  server: {
-    port: 3000,
-  },
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode === 'development' ? '/' : '/Daily-Report-Form/',
+    plugins: [vue()],
+    server: {
+      port: 3000,
+    },
+  };
 });
